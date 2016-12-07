ea24cd277e95153521bff156ffb8cd83634d6395
Nick Turner <dimesteaksNT@gmail.com>
2016-12-07 10:37:16 -0600

    added albums_seeder and inserted values into my albums table.  moved a couple  table values around in albums_migration

--- database_exercise/albums_migration.sql
+++ database_exercise/albums_migration.sql
@@ -4,11 +4,14 @@ CREATE TABLE albums (
   id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist       VARCHAR(50)  NOT NULL,
   name         VARCHAR(100) NOT NULL,
-  release_date YEAR         NOT NULL,
-  sales FLOAT NOT NULL,
+  release_date YEAR,
+  sales_in_millions DECIMAL(8,2) NOT NULL,
   genre        VARCHAR(50)  NOT NULL,
   PRIMARY KEY (id)
 );
 show tables;
 describe albums;
 
+/*Avoid naming columns date
+when dealing with money use decimal type
+phone numbers don't use Int, use strings*/
