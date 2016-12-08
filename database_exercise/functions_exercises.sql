5688859b4bd46882d982378343ee41268cab71f1
njturner <dimesteaksNT@gmail.com>
2016-12-08 15:08:44 -0600

    adding function exercise step 5 and corrections to other steps

--- database_exercise/functions_exercises.sql
+++ database_exercise/functions_exercises.sql
@@ -45,10 +45,30 @@ select AVG(salary) FROM salaries GROUP BY emp_no;
 /*EXERCISE*/
 
 
-SELECT count(*),gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+SELECT count(*) AS 'COUNT',gender AS 'GENDER'
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
 
-SELECT concat(first_name, ' ', last_name ) FROM employees WHERE last_name LIKE ('E%') OR ('%E') ORDER BY emp_no DESC;
+SELECT concat(first_name, ' ', last_name) AS 'FULL NAME'
+FROM employees
+WHERE last_name LIKE ('E%') OR ('%E')
+ORDER BY emp_no DESC;
 
-SELECT *, datediff(now(), hire_date) from employees WHERE birth_date LIKE '%12-25%' AND(hire_date BETWEEN '1990-01-01' AND '1999-12-31') ORDER BY birth_date, hire_date DESC;
+/*or use hire_date like '199%'*/
+SELECT *, datediff(now(), hire_date) AS 'DAYS WORKED'
+FROM employees
+WHERE birth_date LIKE '%12-25%' AND(hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC;
+/*divide datediff by 365 for years*/
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' ;
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY first_name;
+
+SELECT count(*) AS 'repeats',
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY repeats DESC, first_name, last_name;
