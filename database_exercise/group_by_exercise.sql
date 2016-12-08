9e5a6c3bf0ef117d51666eb9303a1558731beac5
njturner <dimesteaksNT@gmail.com>
2016-12-08 10:56:28 -0600

    FINAL group by exercise file question 6,7

--- database_exercise/group_by_exercise.sql
+++ database_exercise/group_by_exercise.sql
@@ -16,3 +16,7 @@ GROUP BY last_name;
 SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' and last_name LIKE '%E';
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
