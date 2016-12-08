27446cc4ef970833c784c2d1b3f186cd8337857c
njturner <dimesteaksNT@gmail.com>
2016-12-08 10:34:51 -0600

    order by exercise 1

--- database_exercise/order_by_exercise.sql
+++ database_exercise/order_by_exercise.sql
@@ -1,8 +1,9 @@
+
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 
-SELECT * FROM employees WHERE first_name='Irena' OR first_name = 'Vidya' OR first_name='Maya';
+SELECT * FROM employees WHERE first_name='Irena' OR first_name = 'Vidya' OR first_name='Maya' ORDER BY first_name;
 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
 
@@ -18,5 +19,4 @@ SELECT * FROM employees WHERE birth_date LIKE '%12-25%' AND( hire_date BETWEEN '
 
 SELECT * FROM employees WHERE last_name LIKE '%e%';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
-
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' ;
