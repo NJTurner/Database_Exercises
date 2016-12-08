84c5c5de62c44901eeb5a7acaf3bfee77ca409dc
njturner <dimesteaksNT@gmail.com>
2016-12-08 10:39:25 -0600

    order by exercise question 3

--- database_exercise/order_by_exercise.sql
+++ database_exercise/order_by_exercise.sql
@@ -5,7 +5,7 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY 
 
 SELECT * FROM employees WHERE first_name='Irena' OR first_name = 'Vidya' OR first_name='Maya' ORDER BY first_name;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M' ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M' ORDER BY last_name, first_name;
 
 SELECT * FROM employees WHERE last_name LIKE ('E%') OR ('%E');
 
